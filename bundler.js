b708f84a2359c622fd424ff40a69dc3c5d19ef59
Lucas Portet <lucasportet@gmail.com>
2023-01-19 23:49:39 +0100

    fix: dpendency injection fixed in tmp

--- bundler.js
+++ bundler.js
@@ -1,21 +1,18 @@
 const os = require('os');
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 const { rollup } = require('rollup');
 const svelte = require('rollup-plugin-svelte');
 const resolve = require('@rollup/plugin-node-resolve');
 
-const emptyFile = path.join(os.tmpdir(), 'App.svelte');
+const tmpdir = os.tmpdir();
+const emptyFile = path.join(tmpdir, 'App.svelte');
 // Ensure we have an empty file at root in functions execution
 fs.writeFileSync(emptyFile, '');
+fs.copySync('./node_modules', path.join(tmpdir, './node_modules'));
 
 module.exports = async function bundler(content, { autorun = false }) {
-  console.log('readdir ' + os.tmpdir());
-  fs.readdirSync(os.tmpdir()).forEach((file) => {
-    console.log(file);
-  });
-
   const bundle = await rollup({
     input: emptyFile,
     plugins: [
